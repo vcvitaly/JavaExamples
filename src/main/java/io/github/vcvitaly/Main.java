5d88ba8e5457266aca8781497e364f73b8913558
Vitalii Chura <vcvitaly@yahoo.com>
2024-03-08 10:22:57 +0200

    Modify main, exclude idea folder

--- src/main/java/io/github/vcvitaly/Main.java
+++ src/main/java/io/github/vcvitaly/Main.java
@@ -1,7 +1,20 @@
 package io.github.vcvitaly;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final Random rand = new Random();
+        for (int i = 0; i < 1000; i++) {
+            final long l = rand.nextLong();
+            BigInteger res = BigInteger.valueOf(l).multiply(BigInteger.valueOf(l));
+            System.out.println(res);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
